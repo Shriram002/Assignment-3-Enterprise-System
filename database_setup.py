e71e013ff20cdba203a7c1602773381ef3db3dcb
Shriram002 <yadavshriram002@gmail.com>
2024-07-05 17:55:16 -0400

    update

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///students.db')
