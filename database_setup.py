19fdbba24d1e66dada3549077ba52ee6e64cacf3
Shriram002 <yadavshriram002@gmail.com>
2024-07-05 18:46:17 -0400

    update

--- database_setup.py
+++ database_setup.py
@@ -16,18 +16,17 @@ class Student(db.Model):
     dob = db.Column(db.Date, nullable=False)
     amount_due = db.Column(db.Float, nullable=False)
 
-@app.before_first_request
+
+@app.before_request
 def init_db():
-    db.create_all()
-
-    # Check if the database is empty before adding initial data
-    if Student.query.count() == 0:
-        initial_students = [
-            Student(first_name='Ajit', last_name='Shinde', dob=datetime.strptime('2000-01-01', '%Y-%m-%d'), amount_due=1000.0),
-            Student(first_name='Sam', last_name='Curran', dob=datetime.strptime('1998-05-12', '%Y-%m-%d'), amount_due=1500.0),
-            Student(first_name='Shriram', last_name='Yadav', dob=datetime.strptime('2001-09-23', '%Y-%m-%d'), amount_due=2000.0)
-        ]
-
-        # Add and commit the initial data
-        db.session.bulk_save_objects(initial_students)
-        db.session.commit()
+    if not hasattr(app, 'db_initialized'):
+        db.create_all()
+        if Student.query.count() == 0:
+            initial_students = [
+                Student(first_name='Ajit', last_name='Shinde', dob=datetime.strptime('2000-01-01', '%Y-%m-%d'), amount_due=1000.0),
+                Student(first_name='Sam', last_name='Curran', dob=datetime.strptime('1998-05-12', '%Y-%m-%d'), amount_due=1500.0),
+                Student(first_name='Shriram', last_name='Yadav', dob=datetime.strptime('2001-09-23', '%Y-%m-%d'), amount_due=2000.0)
+            ]
+            db.session.bulk_save_objects(initial_students)
+            db.session.commit()
+        app.db_initialized = True
